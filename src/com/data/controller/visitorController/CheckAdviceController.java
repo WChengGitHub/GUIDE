2d8d507cb6c5a8992c1ce9d7e99873a4a1ba10d7
WangCheng <154468476@qq.com>
2016-08-02 15:08:33 +0800

    实现了根据后台记录的数目，再前台页面实现，用jqery ajax 实现，加入了font 文件夹，在css包中加入相关的css，用来实现小背景

--- src/com/data/controller/visitorController/CheckAdviceController.java
+++ src/com/data/controller/visitorController/CheckAdviceController.java
@@ -1,14 +1,37 @@
 package com.data.controller.visitorController;
 
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.data.service.visitorService.checkAdviceService.CheckAdvice;
-
+@Controller
 public class CheckAdviceController {
+	@RequestMapping("/getAdviceNumber")
+	@ResponseBody
+	public String getAdviceNumber(HttpServletResponse response) throws IOException
+	{
+		ApplicationContext factory=new ClassPathXmlApplicationContext("applicationContext.xml");
+  	    CheckAdvice checkAdvice=(CheckAdvice) factory.getBean("CheckAdvice");
+  	    System.out.println(checkAdvice.queryAdviceNumber());
+		Writer writer=response.getWriter();
+		int number=checkAdvice.queryAdviceNumber();
+		String json="{\"number\":"+number+"}";
+		writer.write(json);
+		System.out.println("/getAdviceNumber");
+		return null;
+	}
+	/*
       public static void main(String []args){
     	  ApplicationContext factory=new ClassPathXmlApplicationContext("applicationContext.xml");
     	  CheckAdvice checkAdvice=(CheckAdvice) factory.getBean("CheckAdvice");
     	  System.out.println(checkAdvice.queryAdviceNumber());
-      }
+      }*/
 }
